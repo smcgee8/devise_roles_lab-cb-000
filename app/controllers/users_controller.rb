2335ab2d4f38ebb8b9f93b2ad657b7167179160a
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-13 23:46:08 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    return head(:forbidden) unless current_user.admin? || current_user.try(:id) == @post.owner_id
   end
 
 end
